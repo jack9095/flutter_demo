8ecb0ff57d6171e96b60d79367ef70b4cb976205
wangfei <gaodun123com>
2019-01-02 14:50:39 +0800

    常用的widget练习

---
+++ lib/ListViewRefresh.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+//void main() => runApp(new MyScaffold());
+void main() {
+  runApp(new MaterialApp(
+    title: 'My app', // used by the OS task switcher
+    home: new MyScaffold(),
+  ));
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter GestureDetector',
+      home: new ShoppingList(
+        products: <Product>[
+          new Product(name: 'Eggs'),
+          new Product(name: 'Flour'),
+          new Product(name: 'Chocolate chips'),
+        ],
+      ),
+    );
+  }
+}
+
+class Product {
+  const Product({this.name});
+
+  final String name;
+}
+
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({
+    Product product,
+    this.inCart,
+    this.onCartChanged,
+  })  : product = product,
+        super(key: new ObjectKey(product));
+
+  // 该ShoppingListItem widget是无状态的。它将其在构造函​​数中接收到的值存储在final成员变量中，然后在build函数中使用它们。
+  // 例如，inCart布尔值表示在两种视觉展示效果之间切换：一个使用当前主题的主色，另一个使用灰色
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  // 头像的颜色 选中黑色，未选中就是主题颜色
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return new TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      onTap: () {  // item的点击事件
+        onCartChanged(product, !inCart);  // 回调到父控件（widget)）
+      },
+      leading: new CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: new Text(product.name[0]),  // 头像中间是标题的首字母
+      ),
+      title: new Text(product.name, style: _getTextStyle(context)),
+    );
+  }
+}
+
+// ListView
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  final List<Product> products;
+
+  @override
+  _ShoppingListState createState() => new _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = new Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {  //  会为State对象触发build()方法，从而导致对UI的更新
+      print("ListItem 点击 回调");
+      if (inCart)
+        _shoppingCart.add(product);
+      else
+        _shoppingCart.remove(product);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Shopping List'),
+      ),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) { // 某一个item回调
+          return new ShoppingListItem(
+            product: product,  // 哪一个item
+            inCart: _shoppingCart.contains(product),  // 数据集合中是否包含这个 itemData,包含返回true，不包含返回false
+            onCartChanged: _handleCartChanged,  // ListItem 回调
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  @override
+  void initState() {   // 初始化工作 生命周期
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {  // 销毁的时候回调的方法 生命周期
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,197 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-//void main() => runApp(new MyScaffold());
+
 void main() {
-  runApp(new MaterialApp(
-    title: 'My app', // used by the OS task switcher
-    home: new MyScaffold(),
-  ));
+  runApp(new AnimatedListSample());
 }
 
-class MyScaffold extends StatelessWidget {
+class AnimatedListSample extends StatefulWidget {
+  @override
+  _AnimatedListSampleState createState() => new _AnimatedListSampleState();
+}
+
+class _AnimatedListSampleState extends State<AnimatedListSample> {
+  final GlobalKey<AnimatedListState> _listKey = new GlobalKey<AnimatedListState>();
+  ListModel<int> _list;
+  int _selectedItem;
+  int _nextItem; // The next item inserted when the user presses the '+' button.
+
+  @override
+  void initState() {
+    super.initState();
+    _list = new ListModel<int>(
+      listKey: _listKey,
+      initialItems: <int>[0, 1, 2],
+      removedItemBuilder: _buildRemovedItem,
+    );
+    _nextItem = 3;
+  }
+
+  // Used to build list items that haven't been removed.
+  Widget _buildItem(BuildContext context, int index, Animation<double> animation) {
+    return new CardItem(
+      animation: animation,
+      item: _list[index],
+      selected: _selectedItem == _list[index],
+      onTap: () {
+        setState(() {
+          _selectedItem = _selectedItem == _list[index] ? null : _list[index];
+        });
+      },
+    );
+  }
+
+  // Used to build an item after it has been removed from the list. This method is
+  // needed because a removed item remains  visible until its animation has
+  // completed (even though it's gone as far this ListModel is concerned).
+  // The widget will be used by the [AnimatedListState.removeItem] method's
+  // [AnimatedListRemovedItemBuilder] parameter.
+  Widget _buildRemovedItem(int item, BuildContext context, Animation<double> animation) {
+    return new CardItem(
+      animation: animation,
+      item: item,
+      selected: false,
+      // No gesture detector here: we don't want removed items to be interactive.
+    );
+  }
+
+  // Insert the "next item" into the list model.
+  void _insert() {
+    final int index = _selectedItem == null ? _list.length : _list.indexOf(_selectedItem);
+    _list.insert(index, _nextItem++);
+  }
+
+  // Remove the selected item from the list model.
+  void _remove() {
+    if (_selectedItem != null) {
+      _list.removeAt(_list.indexOf(_selectedItem));
+      setState(() {
+        _selectedItem = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter GestureDetector',
-      home: new ShoppingList(
-        products: <Product>[
-          new Product(name: 'Eggs'),
-          new Product(name: 'Flour'),
-          new Product(name: 'Chocolate chips'),
-        ],
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('AnimatedList'),
+          actions: <Widget>[
+            new IconButton(
+              icon: const Icon(Icons.add_circle),
+              onPressed: _insert,
+              tooltip: 'insert a new item',
+            ),
+            new IconButton(
+              icon: const Icon(Icons.remove_circle),
+              onPressed: _remove,
+              tooltip: 'remove the selected item',
+            ),
+          ],
+        ),
+        body: new Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: new AnimatedList(
+            key: _listKey,
+            initialItemCount: _list.length,
+            itemBuilder: _buildItem,
+          ),
+        ),
       ),
     );
   }
 }
 
-class Product {
-  const Product({this.name});
+/// Keeps a Dart List in sync with an AnimatedList.
+///
+/// The [insert] and [removeAt] methods apply to both the internal list and the
+/// animated list that belongs to [listKey].
+///
+/// This class only exposes as much of the Dart List API as is needed by the
+/// sample app. More list methods are easily added, however methods that mutate the
+/// list must make the same changes to the animated list in terms of
+/// [AnimatedListState.insertItem] and [AnimatedList.removeItem].
+class ListModel<E> {
+  ListModel({
+    @required this.listKey,
+    @required this.removedItemBuilder,
+    Iterable<E> initialItems,
+  }) : assert(listKey != null),
+        assert(removedItemBuilder != null),
+        _items = new List<E>.from(initialItems ?? <E>[]);
 
-  final String name;
+  final GlobalKey<AnimatedListState> listKey;
+  final dynamic removedItemBuilder;
+  final List<E> _items;
+
+  AnimatedListState get _animatedList => listKey.currentState;
+
+  void insert(int index, E item) {
+    _items.insert(index, item);
+    _animatedList.insertItem(index);
+  }
+
+  E removeAt(int index) {
+    final E removedItem = _items.removeAt(index);
+    if (removedItem != null) {
+      _animatedList.removeItem(index, (BuildContext context, Animation<double> animation) {
+        return removedItemBuilder(removedItem, context, animation);
+      });
+    }
+    return removedItem;
+  }
+
+  int get length => _items.length;
+  E operator [](int index) => _items[index];
+  int indexOf(E item) => _items.indexOf(item);
 }
 
-typedef void CartChangedCallback(Product product, bool inCart);
+/// Displays its integer item as 'item N' on a Card whose color is based on
+/// the item's value. The text is displayed in bright green if selected is true.
+/// This widget's height is based on the animation parameter, it varies
+/// from 0 to 128 as the animation varies from 0.0 to 1.0.
+class CardItem extends StatelessWidget {
+  const CardItem({
+    Key key,
+    @required this.animation,
+    this.onTap,
+    @required this.item,
+    this.selected: false
+  }) : assert(animation != null),
+        assert(item != null && item >= 0),
+        assert(selected != null),
+        super(key: key);
 
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({
-    Product product,
-    this.inCart,
-    this.onCartChanged,
-  })  : product = product,
-        super(key: new ObjectKey(product));
-
-  // 该ShoppingListItem widget是无状态的。它将其在构造函​​数中接收到的值存储在final成员变量中，然后在build函数中使用它们。
-  // 例如，inCart布尔值表示在两种视觉展示效果之间切换：一个使用当前主题的主色，另一个使用灰色
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  // 头像的颜色 选中黑色，未选中就是主题颜色
-  Color _getColor(BuildContext context) {
-    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
-  }
-
-  TextStyle _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-
-    return new TextStyle(
-      color: Colors.black54,
-      decoration: TextDecoration.lineThrough,
-    );
-  }
+  final Animation<double> animation;
+  final VoidCallback onTap;
+  final int item;
+  final bool selected;
 
   @override
   Widget build(BuildContext context) {
-    return new ListTile(
-      onTap: () {  // item的点击事件
-        onCartChanged(product, !inCart);  // 回调到父控件（widget)）
-      },
-      leading: new CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: new Text(product.name[0]),  // 头像中间是标题的首字母
-      ),
-      title: new Text(product.name, style: _getTextStyle(context)),
-    );
-  }
-}
-
-// ListView
-class ShoppingList extends StatefulWidget {
-  ShoppingList({Key key, this.products}) : super(key: key);
-
-  final List<Product> products;
-
-  @override
-  _ShoppingListState createState() => new _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-  Set<Product> _shoppingCart = new Set<Product>();
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(() {  //  会为State对象触发build()方法，从而导致对UI的更新
-      print("ListItem 点击 回调");
-      if (inCart)
-        _shoppingCart.add(product);
-      else
-        _shoppingCart.remove(product);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Shopping List'),
-      ),
-      body: new ListView(
-        padding: new EdgeInsets.symmetric(vertical: 8.0),
-        children: widget.products.map((Product product) { // 某一个item回调
-          return new ShoppingListItem(
-            product: product,  // 哪一个item
-            inCart: _shoppingCart.contains(product),  // 数据集合中是否包含这个 itemData,包含返回true，不包含返回false
-            onCartChanged: _handleCartChanged,  // ListItem 回调
-          );
-        }).toList(),
+    TextStyle textStyle = Theme.of(context).textTheme.display1;
+    if (selected)
+      textStyle = textStyle.copyWith(color: Colors.lightGreenAccent[400]);
+    return new Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: new SizeTransition(
+        axis: Axis.vertical,
+        sizeFactor: animation,
+        child: new GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: onTap,
+          child: new SizedBox(
+            height: 128.0,
+            child: new Card(
+              color: Colors.primaries[item % Colors.primaries.length],
+              child: new Center(
+                child: new Text('Item $item', style: textStyle),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
